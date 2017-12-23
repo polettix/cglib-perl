846ed997daa7451e86b308c4857939adb338195f
Flavio Poletti <flavio@polettix.it>
2017-12-23 01:07:04 +0100

    Add least common multiple to GCD

--- GCD.pm
+++ GCD.pm
@@ -1,9 +1,10 @@
 package GCD;
 use strict;
 use Exporter qw< import >;
-our @EXPORT_OK = qw< gcd egcd >;
+our @EXPORT_OK = qw< gcd egcd lcm >;
 
 sub gcd { my ($A, $B) = @_; ($A, $B) = ($B % $A, $A) while $A; return $B }
+sub lcm { return ($_[0] / gcd(@_)) * $_[1] }
 
 sub egcd { # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
    my ($X, $x, $Y, $y, $A, $B, $q) = (1, 0, 0, 1, @_);
