4dca81846a8c5a2bd2cfa777f373c54e7691c511
Flavio Poletti <flavio@polettix.it>
2018-01-06 17:28:40 +0100

    Add action on discover for Breadth First Visit

--- BreadthFirstVisit.pm
+++ BreadthFirstVisit.pm
@@ -5,24 +5,28 @@ our @EXPORT_OK = qw< breadth_first_visit >;
 
 sub breadth_first_visit {
    my %args = (@_ && ref($_[0])) ? %{$_[0]} : @_;
-   my @starts = exists($args{start}) ? ($args{start})
-      : exists($args{starts})        ? (@{$args{starts}})
-      :                                die "missing 'start' or 'starts'";
+   my @s = exists($args{start}) ? ($args{start})
+      : exists($args{starts})   ? (@{$args{starts}})
+      :                           die "missing 'start' or 'starts'";
    my $succs = $args{successors} || die "missing parameter 'successors'";
    my $id_of = $args{identifier} || sub { return "$_[0]" };
-   my $action  = $args{action} || undef;
-   my %marked;
-   my @queue = map { $marked{$id_of->($_)} = 1; [$_, undef] } @starts;
-   while (@queue) {
-      my ($v, $pred) = @{shift @queue};  # "dequeue"
-      $action->($v, $pred) if $action;
+   my $va = $args{action} || $args{visit_action} || undef;
+   my $da = $args{discover_action} || undef;
+   my %m; # keep track of marked nodes
+   my @q = map {
+      $da && $da->($_, undef); $m{$id_of->($_)} = 1; [$_, undef] } @s;
+   while (@q) {
+      my ($v, $pred) = @{shift @q};  # "dequeue"
+      $va->($v, $pred) if $va;
       for my $w ($succs->($v)) {
-         push @queue, [$w, $v] unless $marked{$id_of->($w)}++;
+         next if $m{$id_of->($w)}++;
+         $da->($w, $v) if $da;
+         push @q, [$w, $v];
       }
    }
    return unless defined wantarray; # don't bother with void context
-   return keys %marked if wantarray;
-   return [keys %marked] if defined wantarray;
+   return keys %m if wantarray;
+   return [keys %m] if defined wantarray;
 }
 
 1;
