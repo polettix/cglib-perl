34873545d5c0139576e88c479ac06c4b7bbfadcb
Flavio Poletti <flavio@polettix.it>
2018-04-24 15:26:33 +0000

    Rename $a to $av to avoid potential clashing

--- AlphaBeta.pm
+++ AlphaBeta.pm
@@ -9,21 +9,21 @@ sub alpha_beta {
    exists($args{$_}) || die "missing parameter '$_'" for @reqs;
    my ($s, $mf, $md, $e, $d, $f) = @args{@reqs};
    return(wantarray ? ($e->($s), []) : $e->($s)) if ($d == 0) || $f->($s);
-   my $a = $args{alpha} // undef;
+   my $av = $args{alpha} // undef;
    my ($bm, $bv, $hm); # "best" stuff, undef treated as +/- inf
    for my $m ($mf->($s)) {
       ($hm, my $ns) = (1, $md->($s, $m)); # set "has moved" too
       my ($v, $t) = alpha_beta(
          %args,
          alpha => (defined $args{beta} ? -$args{beta} : undef),
-         beta  => (defined $a ? -$a : $a),
+         beta  => (defined $av ? -$av : $av),
          depth => ($d - 1),
       );
       $v = -$v;
       $args{rollback}->($s, $m, $ns) if defined $args{rollback};
       ($bv, $bm) = ($v, [$m, @$t]) if (!defined $bv) || ($v > $bv); # "max"
       last if defined($args{beta}) && $bv >= $args{beta};
-      $a = $bv if (!defined $a) || ($a < $bv);
+      $av = $bv if (!defined $av) || ($av < $bv);
    }
    die "can't handle no_move" unless $hm || defined $args{no_move};
    return $args{no_move}->($s, $d) unless $hm;
