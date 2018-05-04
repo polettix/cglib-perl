7805353d2ede40de8bf8828d7a944bbd4febb12a
Flavio Poletti <flavio@polettix.it>
2018-05-04 09:29:10 +0200

    Advance Bareiss/Matrix, on to kernel

--- Bareiss.pm
+++ Bareiss.pm
@@ -4,32 +4,30 @@ use Exporter qw< import >;
 our @EXPORT_OK = qw< bareiss >;
 
 sub bareiss {    # transforms matrix in-place, hopefully
-   my ($A, $B, $m, $n) = ($_[0], [@{$_[0]}], $_[0][-1], $#{$_[0]} / $_[0][-1]);
-   ($A, $B) = ($B, $A) if (($n & 1) xor (int(($n - 1) / 2) & 2));
+   my ($A, $m, $n) = ($_[0], $_[0][-1], $#{$_[0]} / $_[0][-1]);
    for (my $k = 1; $k <= $n - 1; $k += 2) {
-      ($A, $B) = ($B, $A);
       my $K = ($k - 1) * $m + $k - 1;    # upper-left corner
       if ($k < $n - 1) {
          my $c0 =
-           $B->[$K] * $B->[$K + $m + 1] - $B->[$K + 1] * $B->[$K + $m];
+           $A->[$K] * $A->[$K + $m + 1] - $A->[$K + 1] * $A->[$K + $m];
          for my $i ($k + 1 .. $n - 1) {
             my $I  = $i * $m + $k - 1;
-            my $c1 = $B->[$K + 1] * $B->[$I] - $B->[$K] * $B->[$I + 1];
+            my $c1 = $A->[$K + 1] * $A->[$I] - $A->[$K] * $A->[$I + 1];
             my $c2 =
-              $B->[$K + $m] * $B->[$I + 1] - $B->[$K + $m + 1] * $B->[$I];
+              $A->[$K + $m] * $A->[$I + 1] - $A->[$K + $m + 1] * $A->[$I];
             for my $j ($k + 1 .. $m - 1) {
                $A->[$i * $m + $j] =
-                 $c0 * $B->[$i * $m + $j] +
-                 $c1 * $B->[$k * $m + $j] +
-                 $c2 * $B->[($k - 1) * $m + $j];
+                 $c0 * $A->[$i * $m + $j] +
+                 $c1 * $A->[$k * $m + $j] +
+                 $c2 * $A->[($k - 1) * $m + $j];
             } ## end for my $j ($k + 1 .. $m...)
          } ## end for my $i ($k + 1 .. $n...)
       } ## end if ($k < $n - 1)
       if ($k < $n) {
          for my $l ($k .. $m - 1) {
             $A->[$k * $m + $l] =
-              $B->[$K] * $B->[$k * $m + $l] -
-              $B->[($k - 1) * $m + $l] * $B->[$K + $m];
+              $A->[$K] * $A->[$k * $m + $l] -
+              $A->[($k - 1) * $m + $l] * $A->[$K + $m];
          }
       } ## end if ($k < $n)
    } ## end for (my $k = 1; $k <= $n...)
--- Matrix.pm
+++ Matrix.pm
@@ -1,23 +1,56 @@
 package Matrix;
 use strict;
 use Exporter qw< import >;
-our @EXPORT_OK = qw< M >;
+our @EXPORT_OK = qw< eye M >;
 use overload '""' => \&stringify;
 
 sub clone { return bless [@{$_[0]}], __PACKAGE__ }
+sub eye; # see below
+sub kernel; # see below
 sub idx { return $_[0][-1] * $_[1] + $_[2] }
-sub Mify { return bless $_[0], __PACKAGE__ }
 sub M { return Mify([ref($_[0]) ? @{$_[0]} : @_]) }
+sub Mify { return bless $_[0], __PACKAGE__ }
+sub merge_below; # see below
+sub merge_right { return $_[0]->T->merge_below($_[1]->clone->T)->T }
+sub n_columns { return $_[0][-1] }
+sub n_rows { return $#{$_[0]} / $_[0][-1] }
 sub stringify;    # see below
+sub T; # see below
 sub v { my $i = idx(@_); @_ > 3 ? ($_[0][$i] = $_[3]) : $_[0][$i] }
+
+sub eye {
+   my ($n, $n2, $i) = ($_[0], $_[0] * $_[0], 0);
+   my @array = ((0) x $n2, $n);
+   ($array[$i], $i) = (1, $i + $n + 1) while $i < $n2;
+   return bless \@array, __PACKAGE__;
+}
+
+sub kernel {
+   my ($s, $c, $r) = ($_[0]->clone, $_[0][-1], $#{$_[0]} / $_[0][-1]);
+   my $bareiss = __PACKAGE__->can('bareiss') ||
+      do { require Bareiss; \&Bareiss::bareiss };
+   $s->merge_below(eye($c))->T; # ready for row-echelon
+   return $bareiss->($s);
+}
+
+sub merge_below {
+   my ($s, $t, $ct) = ($_[0], $_[1], $_[1][-1]);
+   die "cannot just_below(), incompatible matrixes" unless pop(@$s) == $ct;
+   @$s = (@$s, @$t);
+   return $s;
+}
 
 sub stringify {
    my ($s, $c, $r) = ($_[0], $_[0][-1], $#{$_[0]} / $_[0][-1]);
    my $l = (sort { $a <=> $b } map { length } @{$s}[0 .. $#$s - 1])[-1];
-   my $inner = join "|\n|", map {
-      join ' ',
-        map { sprintf " %${l}s ", $_ }
-        @{$s}[$_ * $c .. ($_ + 1) * $c - 1]
-   } 0 .. $r - 1;
+   my $inner = join "|\n|", map { join ' ', map { sprintf " %${l}s ", $_ }
+        @{$s}[$_ * $c .. ($_ + 1) * $c - 1] } 0 .. $r - 1;
    return '|' . $inner . '|';
 } ## end sub stringify
+
+sub T {
+   my ($s, $c, $r, $i) = ($_[0], $_[0][-1], $#{$_[0]} / $_[0][-1]);
+   @{$s}[map {$i = $_; map {$i + $_ * $r} 0 .. $c - 1} 0 .. $r - 1] = @$s;
+   $s->[-1] = $r;
+   return $s;
+}
